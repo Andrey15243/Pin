49e70628313afa9516e1c79b17e8fa90ab011922
Andrey <freeskater91@inbox.ru>
2025-08-27 22:15:23 +0500

    Update Stars

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 // ====== Конфиг ======
 const token = process.env.BOT_TOKEN;
-const DOMAIN = process.env.DOMAIN; // твой HTTPS-домен (например https://mybot.selectel.ru)
+const DOMAIN = process.env.DOMAIN;
 const PORT = process.env.PORT || 3000;
 const webAppUrl = "https://pincoinbot.web.app";
 
@@ -31,7 +31,7 @@ async function createBoostInvoice() {
     payload: "boost_payload",
     provider_token: "", // Stars → пустая строка
     currency: "XTR",
-    prices: [{ label: "Boost", amount: 1 }] // 1 Star
+    prices: [{ label: "Boost", amount: 100 }] // 1 Star
   });
 }
 
